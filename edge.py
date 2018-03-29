1441e4c87a97281eb9035b5af2c92aab33d5409b
withlqs <withlqs@gmail.com>
2018-03-29 22:13:40 +0800

    some changes

--- edge.py
+++ edge.py
@@ -4,8 +4,13 @@ import cv2
 
 
 def main():
-    img = cv2.imread('pics/3.jpg', cv2.IMREAD_GRAYSCALE)
-    cv2.imshow('readin', img)
+    img = cv2.imread('pics/3.jpg', cv2.IMREAD_COLOR)
+    # resize
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    print(img)
+    cv2.namedWindow('readin', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('readin', 600, 600)
+    cv2.imshow('readin', hsv)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     pass
